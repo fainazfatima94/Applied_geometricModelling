1e38df83ae310110774884f52b6e85e9ab731b1f
Fainaz Fatima <fainaz.fatima94@gmail.com>
2023-03-03 23:12:06 +0100

    first commit

---
+++ BUILD/demo_DEBUG/qtcsettings.cmake
@@ -0,0 +1,13 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "C:/Qt/Tools/QtCreator/share/qtcreator/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "C:/Qt/5.15.2/msvc2019_64/bin/qmake.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "C:/Qt/5.15.2/msvc2019_64" CACHE "PATH" "" FORCE)
+set("CMAKE_C_COMPILER" "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.33.31629/bin/HostX64/x64/cl.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.33.31629/bin/HostX64/x64/cl.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_TOOLCHAIN_FILE" "C:/vcpkg/scripts/buildsystems/vcpkg.cmake" CACHE "UNINITIALIZED" "" FORCE)
+set("CMAKE_MAKE_PROGRAM" "C:/Qt/Tools/Ninja/ninja.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "" CACHE "STRING" "" FORCE)
+set("gmlib_DIR" "D:/appliedGeometry/BUILD/gmlib_DEBUG/cmake" CACHE "PATH" "" FORCE)
